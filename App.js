924f47d2b2f76430aa81a0412e705c4a561eb8ea
afonsorcarvalho <afonso@diagnostica-ma.com.br>
2020-08-17 15:08:08 -0300

    init commit

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
+//Import React
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//Import Navigators from React Navigation
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//Import all the screens needed
+import SplashScreen from './Screen/SplashScreen';
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
+import AsyncStorage from '@react-native-community/async-storage';
+import Reactotron from 'reactotron-react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+Reactotron
+  .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+  .configure({
+    name: "React Native Demo"
+  })
+  .useReactNative({
+    asyncStorage: true, // there are more options to the async storage.
+    networking: { // optionally, you can turn it off with false.
+      ignoreUrls: /symbolicate/
+    },
+    editor: false, // there are more options to editor
+    errors: { veto: (stackFrame) => false }, // or turn it off with false
+    overlay: false, // just turning off overlay
+  })
+  .connect();
+
+
+
+const Auth = createStackNavigator({
+  //Stack Navigator for Login and Sign up Screen
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+  RegisterScreen: {
+    screen: RegisterScreen,
+    navigationOptions: {
+      title: 'Register',
+      headerStyle: {
+        backgroundColor: '#307ecc',
+      },
+      headerTintColor: '#fff',
+    },
   },
 });
+
+const App = createSwitchNavigator({ 
+  SplashScreen: {
+    /* SplashScreen which will come once for 5 Seconds */
+    screen: SplashScreen,
+    navigationOptions: {
+      /* Hiding header for Splash Screen */
+      headerShown: false,
+    },
+  },
+  Auth: {
+    /* Auth Navigator which include Login Signup will come once */
+    screen: Auth,
+  },
+  DrawerNavigationRoutes: {
+    /* Navigation Drawer as a landing page */
+    screen: DrawerNavigationRoutes,
+    navigationOptions: {
+      /* Hiding header for Navigation Drawer as we will use our custom header */
+      headerShown: false,
+    },
+  },
+});
+
+export default createAppContainer(App);
